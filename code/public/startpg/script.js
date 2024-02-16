14f8254e1c1afb50fb81efd83e132bbcda6ee7da
Carrot Xinyu Liu <xl3138@nyu.edu>
2024-02-16 23:27:44 +0800

    firebasetrial1

--- code/public/startpg/script.js
+++ code/public/startpg/script.js
@@ -1,8 +1,9 @@
+const socket = io('ws://localhost:3500')
 const loginBtn = document.querySelector("#login")
 const visitBtn = document.querySelector("#visit")
 loginBtn.addEventListener('click', () => {
-    
+    socket.emit('login', socket.id.substring(0, 5))
 })
 visitBtn.addEventListener('click', () => {
-    window.location.href = "/code/public/app"
+    window.location.href = "../app"
 })
--- code/server.js
+++ code/server.js
@@ -3,6 +3,7 @@ import { Server } from "socket.io"
 import path from "path"
 import { fileURLToPath } from 'url'
 import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, push, onValue, onChildAdded, onChildChanged, onChildRemoved} from "firebase/database";
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
@@ -15,18 +16,76 @@ const PORT = process.env.PORT || 3500
 //create an express app
 const app = express()
 
+
 //user: the middleware function is executed when the base of the requested path matches path.
 //static: load the static files in the "public" folder
 const publicDir = path.join(__dirname, "public")
 app.use(express.static(publicDir))
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(publicDir, "/startpg"))
-}) 
+
+
 
 //Binds and listens for connections on the specified host and port.
 const expressServer = app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
+
+
+// ---------------------- DATABASE ---------------------- //
+let userNum = 0;
+let userData = [];
+let data_loaded = false;
+
+// Firebase initialization
+const firebaseConfig = {
+    apiKey: "AIzaSyCxkgwdNTOPwhosEk6zNwzXPqasIqIuhCY",
+    authDomain: "b611-185f3.firebaseapp.com",
+    projectId: "b611-185f3",
+    storageBucket: "b611-185f3.appspot.com",
+    messagingSenderId: "557411252910",
+    appId: "1:557411252910:web:d84c2b9c084573f00a8b3f",
+    databaseURL: "https://b611-185f3-default-rtdb.firebaseio.com/"
+};
+
+const provider = new GoogleAuthProvider();
+const fireApp = initializeApp(firebaseConfig)
+const db = getDatabase(fireApp);
+const auth = getAuth(fireApp);
+const dbRef = ref(db, 'userData');
+
+//Read Data
+onValue(dbRef, (data)=>{
+    console.log("!USER ADDED");
+    let username = data.key;
+    let dtArray = data.val();
+    userData.push(dtArray);
+    updateStarCount(postElement, data);
+}) 
+
+//Write Data
+function writeUserData(userId, username, ifAI, room, pos, coreData, seedData) {
+    set(ref(db, 'users/' + userId), {
+        username: username,
+        ifAI: ifAI,
+        room: room,
+        pos : pos,
+        coreData: coreData,
+        seedData: seedData
+    });
+  }
+  
+
+  
+function clearDBReference(refName) {
+    let ref = database.ref(refName);
+// clear out the previous data in the key
+    ref.remove()
+    .then(function() {
+        console.log("! DB Remove succeeded.");
+    })
+    .catch(function(error) {
+        console.log("! DB Remove failed: " + error.message);
+    });
+}
 
 
 // ---------------------- SOCKET ---------------------- //
@@ -56,105 +115,32 @@ io.on('connection', socket => {
         socket.broadcast.emit('message', {texts: `${socket.id.substring(0, 5)} disconnected`, user: `${socket.id}`})
     })
 
+    socket.on('login', (id)=>{
+        console.log(`Visitor ${id} want to login`)
+        signInWithPopup(auth, provider)
+        .then((result) => {
+            // Get Google Access Token to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info. (IdP data available using getAdditionalUserInfo(result))
+            const user = result.user;
+            
+              
+        }).catch((error) => {
+            // Handle Errors
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+
+        });
+        
+    })
 
     //listen for activity
     socket.on('activity', (name)=>{
         socket.broadcast.emit('activity', name)
     })
 })
-
-
-// ---------------------- DATABASE ---------------------- //
-// Firebase initialization
-const firebaseConfig = {
-    apiKey: "AIzaSyCxkgwdNTOPwhosEk6zNwzXPqasIqIuhCY",
-    authDomain: "b611-185f3.firebaseapp.com",
-    projectId: "b611-185f3",
-    storageBucket: "b611-185f3.appspot.com",
-    messagingSenderId: "557411252910",
-    appId: "1:557411252910:web:d84c2b9c084573f00a8b3f"
-};
-
-const provider = new GoogleAuthProvider();
-const fireApp = initializeApp(firebaseConfig)
-const db = getFirestore(fireApp);
-const auth = getAuth(fireApp);
-// const firestore = firebase.firestore();
-
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // Get Google Access Token to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info. (IdP data available using getAdditionalUserInfo(result))
-    const user = result.user;
-  }).catch((error) => {
-    // Handle Errors
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    
-  });
-
-
-function getDBReference(refName) {
-    let ref = database.ref(refName);
-    // RefVal.push(ref);
-    // event listeners
-    ref.on("child_added", data => {
-      console.log("!DB ADDED");
-      console.log(data.key);
-      console.log(data.val());
-      let Value = data.val();
-
-      if (data.key == "user") {
-        for (key in Value) {
-          user.push(Value[key]);
-          TextL.push(key);
-        }
-      } else if (data.key == "coreData") {
-        for (key in Value) {
-          coreData.push(Value[key]);
-          TextT.push(key);
-        } 
-      }else if(data.key == "seedData"){
-            
-      }
-      processData();
-    });
-  
-    // console.log(ActivityL.length, ActivityT.length);
-    ref.on("child_removed", data => {
-      console.log("!DB REMOVED");
-      console.log(data.key);
-      console.log(data.val());
-    });
-    ref.on("child_changed", data => {
-      console.log("!DB CHANGED");
-      console.log(data.key);
-      console.log(data.val());
-    });
-    ref.on("child_moved", data => {
-      console.log("!DB MOVED");
-      console.log(data.key);
-      console.log(data.val());
-    });
-    return ref;
-  }
-  
-  function clearDBReference(refName) {
-    let ref = database.ref(refName);
-  
-    // clear out the previous data in the key
-    ref
-      .remove()
-      .then(function() {
-        console.log("! DB Remove succeeded.");
-      })
-      .catch(function(error) {
-        console.log("! DB Remove failed: " + error.message);
-      });
-  }
